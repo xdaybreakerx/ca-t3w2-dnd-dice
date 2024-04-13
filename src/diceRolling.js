35749116bd25ebe120826f9a5a060ecf56802604
Xander Salathe <xsalathe@gmail.com>
2024-04-13 10:57:33 +1000

    docs: update comments for diceRolling.js

--- src/diceRolling.js
+++ src/diceRolling.js
@@ -2,13 +2,14 @@
 
 
 
+
 /**
- * Description placeholder
- * @date 26o266nt().for26amt('2024-04-13 10:26:08')
+ * Generate a random number between 1 and a given size
+ * @date 2024-04-13 10:54:47
  * @author Xander
  *
  * @param {number} [diceSize=6]
- * @returns {*} Generates a random number representing the result of rolling a dice with the specified size. If no size is provided, it defaults to a 6-sided dice.
+ * @returns {*} Generates a random number representing the result of rolling a dice with a given size. If no size is provided, it defaults to a 6-sided dice.
  */
 function rollDice(diceSize = 6){
 	let rollResult = Math.floor(Math.random() * diceSize) + 1;
@@ -17,11 +18,14 @@ function rollDice(diceSize = 6){
 }
 
 
+
 /**
- * Generate a random number between 1 and a given size twice, and identifying the highest number.
+ * Generate a random number between 1 and a given size twice, and identifying the highest number
+ * @date 2024-04-13 10:55:03
+ * @author Xander
  *
- * @param [diceSize=6]
- * @returns a random number between 1 and a given size twice, and identifying the highest number.
+ * @param {number} [diceSize=6]
+ * @returns {{ finalResult: any; rolls: {}; }} A function that simulates rolling two dice with advantage and returns an object containing the final result and the individual dice rolls.
  */
 function rollDiceWithAdvantage(diceSize = 6){
     let result = {
@@ -39,11 +43,14 @@ function rollDiceWithAdvantage(diceSize = 6){
     return result;
 }
 
+
 /**
- * Generate a random number between 1 and a given size twice, and identifying the smallest number.
+ * Generate a random number between 1 and a given size twice, and identifying the smallest number
+ * @date 2024-04-13 10:55:29
+ * @author Xander
  *
- * @param [diceSize=6]
- * @returns a random number between 1 and a given size twice, and identifying the smallest number.
+ * @param {number} [diceSize=6]
+ * @returns {{ finalResult: any; rolls: {}; }} A function that simulates rolling two dice with disadvantage and returns the result of the lowest roll.
  */
 function rollDiceWithDisadvantage(diceSize = 6){
     let result = {
